57e62b4715b4f747a79aca6c2d5f09bc7af5fc25
welerson <welerson.stofelli@gmail.com>
2023-07-23 17:38:15 -0300

    Adicionado: deletePerson

---
+++ GITSENAIapp/src/main/java/com/example/gitsenaiapp/GitsenaIappApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitsenaiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitsenaIappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitsenaIappApplication.class, args);
+    }
+
+}
---
+++ GITSENAIapp/src/main/java/com/example/gitsenaiapp/Pessoa.java
@@ -0,0 +1,33 @@
+package com.example.gitsenaiapp;
+
+public class Pessoa {
+    Integer id;
+    private String name;
+
+    private String sexo;
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
+
